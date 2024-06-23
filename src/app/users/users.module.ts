df34d96f93c0c239653fec0df5dcecdd6e72f875
jjribeiro1 <jvsr.99@hotmail.com>
2024-06-23 17:00:35 -0300

    feat: adds CryptoService hasher to user password

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { CryptoModule } from 'src/app/crypto/crypto.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CryptoModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
 })
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,13 +1,22 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
+import { CryptoService } from 'src/app/crypto/crypto.service';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly cryptoService: CryptoService,
+  ) {}
 
   async create(dto: CreateUserDto) {
-    return this.usersRepository.create(dto);
+    const { email, name, password } = dto;
+    return this.usersRepository.create({
+      email,
+      name,
+      password: await this.cryptoService.hasher(password, 10),
+    });
   }
 
   async findOne(id: string) {

1618f2705897bdc84144fc09b807047d995894f2
jjribeiro1 <jvsr.99@hotmail.com>
2024-06-22 23:19:47 -0300

    refactor: change folder structure

--- test/mocks/user/index.ts
+++ test/mocks/user/index.ts
@@ -1,4 +1,4 @@
-import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { CreateUserDto } from 'src/app/users/dto/create-user.dto';
 
 export const mockCreateUserDto: CreateUserDto = {
   name: 'any-name',

7ea1a194ce514a9e732a5cabcf60fe1a290a3e84
jjribeiro1 <jvsr.99@hotmail.com>
2024-06-23 14:41:32 -0300

    feat: findOne UsersRepository

--- src/app/users/users.repository.ts
+++ src/app/users/users.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersRepository {
@@ -9,5 +10,22 @@ export class UsersRepository {
   async create(data: CreateUserDto): Promise<string> {
     const result = await this.prisma.user.create({ data });
     return result.id;
+  }
+
+  async findOne(id: string) {
+    const result = await this.prisma.user.findUnique({ where: { id } });
+
+    if (!result) {
+      return null;
+    }
+
+    return new User(
+      result.id,
+      result.name,
+      result.email,
+      result.password,
+      result.createdAt,
+      result.updatedAt,
+    );
   }
 }

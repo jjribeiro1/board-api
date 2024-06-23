60b5d87e61ddb478b49de220f3e40f99ff065afa
jjribeiro1 <jvsr.99@hotmail.com>
2024-06-23 17:48:59 -0300

    feat: nest generate auth resource

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { CryptoModule } from './crypto/crypto.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, CryptoModule],
+  imports: [PrismaModule, UsersModule, CryptoModule, AuthModule],
 })
 export class AppModule {}

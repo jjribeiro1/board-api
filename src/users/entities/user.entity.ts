e37be53dada9ff0e21dca695a8c716e7b47fc2c8
jjribeiro1 <jvsr.99@hotmail.com>
2024-06-22 21:42:11 -0300

    feat: user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,10 @@
+export class User {
+  constructor(
+    readonly id: string,
+    readonly name: string,
+    readonly email: string,
+    readonly password: string,
+    readonly createdAt: Date,
+    readonly updatedAt: Date,
+  ) {}
+}

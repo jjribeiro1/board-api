72880edc9ffaf50d438a1278a36cfeb82873ac18
jjribeiro1 <jvsr.99@hotmail.com>
2024-06-23 16:26:38 -0300

    feat: add crypto module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { CryptoModule } from './crypto/crypto.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, CryptoModule],
 })
 export class AppModule {}

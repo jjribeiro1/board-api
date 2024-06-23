9b6ea8ebe7f0223299d62c0bf0005c9c7d5b5799
jjribeiro1 <jvsr.99@hotmail.com>
2024-06-22 23:27:30 -0300

    refactor: change CreateUserDto error messages

--- src/app/users/dto/create-user.dto.ts
+++ src/app/users/dto/create-user.dto.ts
@@ -1,14 +1,15 @@
-import { IsString, IsEmail, Length } from 'class-validator';
+import { IsString, IsEmail, Length, MaxLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
-  @Length(3, 200)
+  @IsString({ message: 'Nome inválido' })
+  @Length(3, 200, { message: 'Nome deve ter entre 3 e 200 caracteres' })
   name: string;
 
-  @IsEmail()
+  @IsEmail({}, { message: 'Email inválido' })
+  @MaxLength(200, { message: 'Email não pode ter mais que 200 caracteres' })
   email: string;
 
-  @IsString()
-  @Length(6, 200)
+  @IsString({ message: 'Senha inválida' })
+  @Length(6, 200, { message: 'Senha deve ter entre 6 e 200 caracteres' })
   password: string;
 }

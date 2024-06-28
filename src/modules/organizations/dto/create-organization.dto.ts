185e3bf300699f3d41124be229e91b0ce5d35dc4
jjribeiro1 <jvsr.99@hotmail.com>
2024-06-28 17:28:59 -0300

    docs: CreateOrganizationDto swagger docs

--- src/modules/organizations/dto/create-organization.dto.ts
+++ src/modules/organizations/dto/create-organization.dto.ts
@@ -1,10 +1,17 @@
 import { IsOptional, IsString, Length } from 'class-validator';
 
 export class CreateOrganizationDto {
+  /**
+   * Name of organization
+   * @example 'Meta'
+   */
   @IsString({ message: 'Nome inválido' })
   @Length(1, 140, { message: 'Nome deve ter entre 1 e 140 caracteres' })
   name: string;
 
+  /**
+   * Url of logo image
+   */
   @IsString({ message: 'Url inválida' })
   @IsOptional()
   logoUrl: string | null;

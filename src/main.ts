ab391a024a9179e6717957e23217a081b4a50332
jjribeiro1 <jvsr.99@hotmail.com>
2024-06-29 22:32:17 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:3000'],
+      credentials: true,
+    },
+  });
   const configService = app.get(ConfigService);
   const PORT = configService.get('SERVER_PORT');
 

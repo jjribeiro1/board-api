cda1d3d2d8505cea0e3c70af4f8b2013f7de8f37
jjribeiro1 <jvsr.99@hotmail.com>
2024-08-18 11:39:46 -0300

    feat: board entity

---
+++ src/modules/boards/entities/board.entity.ts
@@ -0,0 +1,13 @@
+export class Board {
+  constructor(
+    readonly id: string,
+    readonly title: string,
+    readonly description: string,
+    readonly isPrivate: boolean,
+    readonly isLocked: boolean,
+    readonly organizationId: string,
+    readonly authorId: string,
+    readonly createdAt: Date,
+    readonly updatedAt: Date,
+  ) {}
+}

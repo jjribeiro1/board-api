440bb9268a2a4bc2b5766eb2526f22badfee99e3
jjribeiro1 <jvsr.99@hotmail.com>
2024-10-27 17:16:39 -0300

    feat: Comment entity

--- src/modules/comments/entities/comment.entity.ts
+++ src/modules/comments/entities/comment.entity.ts
@@ -1,1 +1,10 @@
-export class Comment {}
+export class Comment {
+  constructor(
+    readonly content: string,
+    readonly authorId: string,
+    readonly postId: string,
+    readonly createdAt: Date,
+    readonly updatedAt: Date,
+    readonly deletedAt: Date | null,
+  ) {}
+}

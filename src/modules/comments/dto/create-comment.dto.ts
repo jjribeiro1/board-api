2675ce872cabf2f28a372b1cdd48c3ac7429d700
jjribeiro1 <jvsr.99@hotmail.com>
2024-10-27 17:14:22 -0300

    feat: CreateCommentDto

--- src/modules/comments/dto/create-comment.dto.ts
+++ src/modules/comments/dto/create-comment.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCommentDto {}
+import { IsNotEmpty, IsUUID } from 'class-validator';
+
+export class CreateCommentDto {
+  /**
+   * content of comment
+   * @example 'I like this suggestion'
+   */
+  @IsNotEmpty({ message: 'Comentário não pode estar vazio' })
+  content: string;
+
+  /**
+   * post ID
+   * @example '0f3b269a-447f-49dc-a804-54585c0fb106'
+   */
+  @IsUUID('4', { message: 'ID da postagem deve ser um UUID válido' })
+  postId: string;
+}

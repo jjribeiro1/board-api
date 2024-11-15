201aa29f86ca75d9119502a915438d471cff6634
jjribeiro1 <jvsr.99@hotmail.com>
2024-11-15 17:18:25 -0300

    chore: setup cookie parser

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PrismaClientExceptionFilter } from './common/filters/prisma-exception.filter';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -23,6 +24,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  app.use(cookieParser());
   app.useGlobalFilters(new PrismaClientExceptionFilter());
 
   const config = new DocumentBuilder()

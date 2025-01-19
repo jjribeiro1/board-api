018eba8179907578e76a231b503ddd62879dde27
jjribeiro1 <jvsr.99@hotmail.com>
2025-01-18 22:10:13 -0300

    chore: setup Events module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { validate } from 'src/config/env.validation';
 import { AuthModule } from './shared/modules/auth/auth.module';
 import { CryptoModule } from './shared/modules/crypto/crypto.module';
 import { PrismaModule } from './shared/modules/database/prisma/prisma.module';
@@ -7,9 +8,9 @@ import { UsersModule } from './modules/users/users.module';
 import { OrganizationsModule } from './modules/organizations/organizations.module';
 import { BoardsModule } from './modules/boards/boards.module';
 import { PostsModule } from './modules/posts/posts.module';
-import { validate } from 'src/config/env.validation';
 import { CommentsModule } from './modules/comments/comments.module';
 import { StatusModule } from './modules/status/status.module';
+import { EventsModule } from './modules/events/events.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { StatusModule } from './modules/status/status.module';
       isGlobal: true,
       validate,
     }),
+    EventsModule,
     PrismaModule,
     AuthModule,
     CryptoModule,

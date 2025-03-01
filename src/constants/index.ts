3c0e806f5794afd77aef8d74bc8e7065c73554d4
jjribeiro1 <jvsr.99@hotmail.com>
2025-03-01 12:17:12 -0300

    refactor: change expiration time from tokens

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,4 @@
-export const JWT_ACCESS_TOKEN_EXPIRES_IN = 60 * 15; // 15 minutes in seconds
-export const JWT_REFRESH_TOKEN_EXPIRES_IN = 60 * 60 * 24 * 7; // 7 days in seconds
-
-export const COOKIE_JWT_ACCESS_TOKEN_EXPIRES_IN = JWT_ACCESS_TOKEN_EXPIRES_IN * 1000; // 15 minutes in milisseconds
-export const COOKIE_JWT_REFRESH_TOKEN_EXPIRES_IN = JWT_REFRESH_TOKEN_EXPIRES_IN * 1000; // 7 day in milisseconds
+export const ACCESS_TOKEN_EXPIRES_IN = 60 * 60;
+export const REFRESH_TOKEN_EXPIRES_IN = 60 * 60 * 24 * 7;
+export const COOKIE_ACCESS_TOKEN_EXPIRES_IN = ACCESS_TOKEN_EXPIRES_IN * 1000;
+export const COOKIE_REFRESH_TOKEN_EXPIRES_IN = REFRESH_TOKEN_EXPIRES_IN * 1000;

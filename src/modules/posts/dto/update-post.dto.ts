76ce0068757bfcf1251fd2930a7ea0ff8e34616e
jjribeiro1 <jvsr.99@hotmail.com>
2025-07-21 21:07:32 -0300

    refactor: change fields of UpdatePostDto

--- src/modules/posts/dto/update-post.dto.ts
+++ src/modules/posts/dto/update-post.dto.ts
@@ -1,4 +1,21 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePostDto } from './create-post.dto';
+import { IsOptional, IsString, Length } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto {
+  /**
+   * post title
+   * @example 'Add feature X'
+   */
+  @IsOptional()
+  @IsString({ message: 'Título inválido' })
+  @Length(3, 200, { message: 'Título deve ter entre 3 e 200 caracteres' })
+  title?: string;
+
+  /**
+   * post description
+   * @example 'Feature X will solve many problems because of this'
+   */
+  @IsOptional()
+  @IsString({ message: 'Descrição inválida' })
+  @Length(3, 300, { message: 'Descrição deve ter entre 3 e 300 caracteres' })
+  description?: string;
+}

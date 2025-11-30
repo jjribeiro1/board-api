8ac4bd6aa6d9097547a3468b3fde99316302055d
jjribeiro1 <jvsr.99@hotmail.com>
2025-11-30 15:40:01 -0300

    fix: update import paths for PrismaClient to use relative paths

--- src/shared/modules/database/prisma/prisma-client-mock.ts
+++ src/shared/modules/database/prisma/prisma-client-mock.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../../../../generated/prisma/client';
+import { PrismaClient } from 'generated/prisma/client';
 import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 
 export type MockContext = {

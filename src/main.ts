f2ba4345330dcb3f672f45e2bd847e5086342314
jjribeiro1 <jvsr.99@hotmail.com>
2025-12-23 16:37:07 -0300

    fix: update server port configuration to use 'PORT' instead of 'SERVER_PORT'

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
-  const PORT = configService.get('SERVER_PORT');
+  const PORT = configService.get('PORT');
 
   app.enableCors({
     credentials: true,

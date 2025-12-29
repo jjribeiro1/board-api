b7fd93bb14034bfa184de8f90aa990272c4f9482
jjribeiro1 <jvsr.99@hotmail.com>
2025-12-29 10:26:00 -0300

    fix: set default port to 3001 if not specified in environment

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
-  const PORT = configService.get('PORT');
+  const PORT = configService.get('PORT') || 3001;
 
   app.enableCors({
     credentials: true,

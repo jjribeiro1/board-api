6a0c2dd45d2f10ecad3bf2123c717eb2d53d136f
João Vitor Ribeiro <jvsr.99@hotmail.com>
2026-05-29 17:15:18 -0300

    fix: update database adapter configuration and remove unused dependencies

--- src/shared/modules/database/prisma/prisma.service.ts
+++ src/shared/modules/database/prisma/prisma.service.ts
@@ -1,17 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from 'src/generated/prisma/client';
-import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor(configService: ConfigService) {
-    const environment = configService.get('NODE_ENV');
-    const isProduction = environment === 'production';
-    const adapter = isProduction
-      ? new PrismaNeon({ connectionString: configService.get('DATABASE_URL') })
-      : new PrismaPg({ connectionString: configService.get('DATABASE_URL') });
+    const adapter = new PrismaPg({ connectionString: configService.get('DATABASE_URL') });
     super({ adapter });
   }
 }
